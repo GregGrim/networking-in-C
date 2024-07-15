2da3caaba9e946a21bc300b3cc8d8b4300e06ab1
hryhorii <grimy.grigory@gmail.com>
2024-07-15 18:32:43 +0200

    headers

--- sock_init.h
+++ sock_init.h
@@ -16,19 +16,3 @@
 #include <errno.h>
 
 #endif
-
-#include <stdio.h>
-int main() {
-#if defined(_WIN32)
-    WSADATA d;
-    if (WSAStartup(MAKEWORD(2, 2), &d)) {
-        fprintf(stderr, "Failed to initialize.\n");
-        return 1;
-    }
-#endif
-    printf("Ready to use socket API.\n");
-#if defined(_WIN32)
-    WSACleanup();
-#endif
-    return 0;
-}
